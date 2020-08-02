79a9d4574b87a467c4090a9c83d72bdb38bfe60d
Alex <alex@MacBook-Pro-2.local>
2020-08-02 13:08:22 +0300

    chalk colors added

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const chalk = require(`chalk`);
 const fs = require(`fs`);
 const {ExitCode} = require(`../../constants`);
 
@@ -72,7 +73,7 @@ module.exports = {
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
     if (countOffer > 1000) {
-      console.error(`Не больше 1000 объявлений`);
+      console.error(chalk.red(`Не больше 1000 объявлений`));
       process.exit(ExitCode.error);
     }
 
@@ -80,10 +81,10 @@ module.exports = {
 
     fs.writeFile(FILE_NAME, content, (err) => {
       if (err) {
-        return console.error(`Can't write data to file...`);
+        return console.error(chalk.red(`Can't write data to file...`));
       }
 
-      return console.info(`Operation success. File created.`);
+      return console.info(chalk.green(`Operation success. File created.`));
     });
   }
 };
