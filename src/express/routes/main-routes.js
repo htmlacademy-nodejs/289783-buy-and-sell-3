321fc2f7c704cbac9eb44aa4402856925c90e559
Alex <alex@MacBook-Pro-2.local>
2020-08-08 14:05:37 +0300

    express implementation

---
+++ src/express/routes/main-routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const {Router} = require(`express`);
+const mainRouter = new Router();
+
+mainRouter.get(`/`, (req, res) => res.send(`/`));
+mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+
+module.exports = mainRouter;
